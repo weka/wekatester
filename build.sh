210e7592ba5889db8a7085e026e003f340517001
Vince Fleming <vince@weka.io>
2025-10-07 17:20:24 +0000

    Fix args

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ pyinstaller --clean --onefile wekatester.py
 TARGET=tarball/wekatester
 mkdir -p $TARGET
 cp dist/wekatester $TARGET
-cp fio $TARGET
+#cp fio $TARGET
 cp -r fio-jobfiles $TARGET
 cp README.md $TARGET
 cd tarball
